5394736b6dd2a90da815f5782d08b9f15b975bd2
rodrigoflores2411 <rodrigoflores12345678@gmail.com>
2025-11-14 11:01:42 -0500

    Delete iac directory

--- iac/lambda_src/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  console.log("Received event", JSON.stringify(event));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Notifier executed" })
-  };
-};
